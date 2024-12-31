e13bdab0200e17dd6127f975c36dfc7489578240
Alexis Guzman <codingcodax@gmail.com>
2024-12-31 17:35:00 -0600

    feat(helpers): create parse hex colors

---
+++ src/helpers/parse-hex-colors.ts
@@ -0,0 +1,12 @@
+export const parseHexColor = (
+  hex: string,
+): [number, number, number] | undefined => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (!result) return undefined;
+
+  return [
+    parseInt(result[1], 16) / 255,
+    parseInt(result[2], 16) / 255,
+    parseInt(result[3], 16) / 255,
+  ];
+};
